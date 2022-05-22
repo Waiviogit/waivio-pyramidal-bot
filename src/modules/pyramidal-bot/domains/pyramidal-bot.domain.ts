a9e01d13ada275608ba579123d861ebfc041fe5e
MashaYakovenko <mariiay@wizardsdev.com>
2022-05-22 23:54:44 +0300

    test purpose

--- src/modules/pyramidal-bot/domains/pyramidal-bot.domain.ts
+++ src/modules/pyramidal-bot/domains/pyramidal-bot.domain.ts
@@ -92,12 +92,12 @@ export class PyramidalBotDomain implements IPyramidalBotDomain {
     }
 
     const tradeFeeMul = _.get(params, '[0].tradeFeeMul', POOL_FEE);
-    this._recalculateQuantities({
-      triggers,
-      pools: pools as unknown as marketPoolType[],
-      tradeFeeMul,
-      slippage: bot.slippage,
-    });
+    // this._recalculateQuantities({
+    //   triggers,
+    //   pools: pools as unknown as marketPoolType[],
+    //   tradeFeeMul,
+    //   slippage: bot.slippage,
+    // });
     const poolsWithToken = _.filter(
       pools,
       (pool) => !_.includes(bot.stablePair, pool.tokenPair),
